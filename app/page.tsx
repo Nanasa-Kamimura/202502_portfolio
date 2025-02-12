1b1d3cbd9a8a4869f3866aecc2235fd55a09cc73
Nanasa-Kamimura <114054528+Nanasa-Kamimura@users.noreply.github.com>
2025-02-12 15:42:33 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "../components/Hero"
-import About from "../components/About"
-import Works from "../components/Works"
-import Skills from "../components/Skills"
-import Contact from "../components/Contact"
-import PageIndicator from "../components/PageIndicator"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Works from "./components/Works"
+import Skills from "./components/Skills"
+import Contact from "./components/Contact"
+import PageIndicator from "./components/PageIndicator"
 
 export default function Home() {
   return (
