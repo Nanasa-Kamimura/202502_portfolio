4335f9f69ab7f707d62f1f6f8cad1a4844e12c01
Nanasa-Kamimura <114054528+Nanasa-Kamimura@users.noreply.github.com>
2025-02-12 11:50:57 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/02502_portfolio',
-  assetPrefix: '/02502_portfolio/',
-  trailingSlash: true,
-};
-
 module.exports = nextConfig;
 
 let userConfig = undefined
