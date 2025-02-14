0e278c9221992e76afe1306418a401c93db46e88
Nanasa-Kamimura <114054528+Nanasa-Kamimura@users.noreply.github.com>
2025-02-14 12:24:20 +0900

    ナビゲーションleftを5

--- app/components/PageIndicator.tsx
+++ app/components/PageIndicator.tsx
@@ -27,7 +27,7 @@ export default function PageIndicator() {
   }, [])
 
   return (
-    <div className="fixed left-３ top-1/2 -translate-y-1/2 z-50 flex flex-col gap-4">
+    <div className="fixed left-5 top-1/2 -translate-y-1/2 z-50 flex flex-col gap-4">
       {sections.map((_, index) => (
         <div
           key={index}
